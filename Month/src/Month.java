aed38011d35bf1456efeb747880d04fe1009b877
Peter Tyonum <ptyonum@renmoney.com>
2020-06-09 08:51:41 +0100

    Return month name by given int representation and vice versa

---
+++ Month/src/Month.java
@@ -0,0 +1,39 @@
+public class Month {
+
+    public static int getMonth(String month){
+        month = month.toLowerCase();
+        switch (month) {
+            case "jan": return 1;
+            case "feb": return 2;
+            case "mar": return 3;
+            case "apr": return 4;
+            case "may": return 5;
+            case "jun": return 6;
+            case "jul": return 7;
+            case "aug": return 8;
+            case "sep": return 9;
+            case "oct": return 10;
+            case "nov": return 11;
+            case "dec": return 12;
+            default: return -1;
+        }
+    }
+
+    public static String getMonth(int month){
+        switch (month){
+            case 1: return "January";
+            case 2: return "February";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+            default: return "Invalid month number representation";
+        }
+    }
+}
